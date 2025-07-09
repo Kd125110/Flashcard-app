4e58d7eb5d7d1b3a8c7d1be5188c45172f7d1ff5
Kd125110 <karol.dabrowski@capgemini.com>
2025-07-09 06:08:50 +0200

    More working routings

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,8 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
+import DashboardPage from './pages/DashboardPage';
+import AddFlashcardPage from './pages/AddFlashcardPage';
+import RegisterPage from './pages/RegisterPage';
 
 function App() {
   return (
@@ -7,6 +10,11 @@ function App() {
       <Routes>
         <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/add-flashcard" element={<AddFlashcardPage />}/>
+        <Route path="/register" element={<RegisterPage />}/>
+        {/* You can add more routes here as needed */}
+        {/* <Route path="/add-flashcard" element={<AddFlashcardPage />} /> */}
         {/* Add other routes here as needed */}
       </Routes>
     </Router>
