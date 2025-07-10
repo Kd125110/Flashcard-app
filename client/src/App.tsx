c273e889bf5e386386ee7edbfcb11a143d94eec1
Kd125110 <dabrowskik290@gmail.com>
2025-07-10 15:20:58 +0200

    Added more routings

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,10 @@ import LoginPage from './pages/LoginPage';
 import DashboardPage from './pages/DashboardPage';
 import AddFlashcardPage from './pages/AddFlashcardPage';
 import RegisterPage from './pages/RegisterPage';
+import ShowFlashcardSets from './pages/ShowFlashcardSets';
+import GuessFlashcard from './pages/GuessFlashcard';
+import EditFlashcardPage from './pages/EditFlashcardPage';
+import UserSettingPage from './pages/UserSettingPage';
 
 function App() {
   return (
@@ -13,6 +17,10 @@ function App() {
         <Route path="/dashboard" element={<DashboardPage />} />
         <Route path="/add-flashcard" element={<AddFlashcardPage />}/>
         <Route path="/register" element={<RegisterPage />}/>
+        <Route path="/show-flashcards-sets" element={<ShowFlashcardSets />} />
+        <Route path="/guess" element={<GuessFlashcard />} />
+        <Route path="/edit" element= {<EditFlashcardPage/>} />
+        <Route path="/usersetting" element = {<UserSettingPage/>} />
         {/* You can add more routes here as needed */}
         {/* <Route path="/add-flashcard" element={<AddFlashcardPage />} /> */}
         {/* Add other routes here as needed */}
