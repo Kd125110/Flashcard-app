f9f927b3874af92c2e822db58e3692767a430147
Kd125110 <karol.dabrowski@capgemini.com>
2025-07-10 15:27:43 +0200

    Created get method

--- server/controllers/flashcardController.js
+++ server/controllers/flashcardController.js
@@ -1,8 +1,10 @@
+import e from "express";
+
 export const addFlashcard = async (req, res) => {
   const db = req.db;
   await db.read();
 
-  const { question, answer, category } = req.body;
+  const { question, answer, category, sourceLang, targetLang } = req.body;
 
   if (!question || !answer || !category) {
     return res.status(400).json({ message: 'Wszystkie pola są wymagane.' });
@@ -13,10 +15,21 @@ export const addFlashcard = async (req, res) => {
     question,
     answer,
     category,
+    sourceLang,
+    targetLang,
   };
 
   db.data.flashcards.push(newFlashcard);
   await db.write();
-
   res.status(201).json({ message: 'Dodano fiszkę!', flashcard: newFlashcard });
 };
+
+
+
+  export const getFlashcard = async (req, res) => {
+    const db = req.db;
+    await db.read();
+
+    const flashcards = db.data.flashcards || [];
+    res.status(200).json({flashcards});
+  }
