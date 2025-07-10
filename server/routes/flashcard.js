406ef485faf39cbc52214298837612f3727a3007
Kd125110 <dabrowskik290@gmail.com>
2025-07-10 15:28:32 +0200

    Added routing

--- server/routes/flashcard.js
+++ server/routes/flashcard.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { addFlashcard } from '../controllers/flashcardController.js';
+import { addFlashcard, getFlashcard } from '../controllers/flashcardController.js';
 
 const router = express.Router();
 
 router.post('/add', addFlashcard);
 
+router.get('/', getFlashcard)
 export default router;
